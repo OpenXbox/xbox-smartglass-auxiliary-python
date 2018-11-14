d24989b69ce41a16413569d82bd8d1b2fbb4938e
tuxuser <462620+tuxuser@users.noreply.github.com>
2018-11-14 15:49:05 +0100

    Update for python 3.7

--- setup.py
+++ setup.py
@@ -26,10 +26,11 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
     ],
     install_requires=[
-        'xbox-smartglass-core>=1.0.8'
+        'xbox-smartglass-core>=1.0.12'
     ],
     tests_require=[
         'pytest',
